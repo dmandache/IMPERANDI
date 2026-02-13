405bb76adc20d104fefaf6223c2144bdcc9d4560
dmandache <diana.mandache00@gmail.com>
2026-02-13 20:38:09 +0100

    format change in report patients

--- src/imperandi/utils/misc.py
+++ src/imperandi/utils/misc.py
@@ -36,9 +36,9 @@ def report_volumes(df, step_name=None):
     unique_counts = df[["patient_key", "study_id", "series_id"]].nunique()
     if step_name:
         logger.info("After %s:", step_name)
-    logger.info("Unique patients: %s", unique_counts["patient_key"])
-    logger.info("Unique studies: %s", unique_counts["study_id"])
-    logger.info("Unique series: %s", unique_counts["series_id"])
+    logger.info("\tUnique patients: %s", unique_counts["patient_key"])
+    logger.info("\tUnique studies: %s", unique_counts["study_id"])
+    logger.info("\tUnique series: %s", unique_counts["series_id"])
 
     if "volume_id" in df.columns:
         unique_volumes = df["volume_id"].nunique()
