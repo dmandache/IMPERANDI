18c619436de99104f693905863f689abe2656928
dmandache <diana.mandache00@gmail.com>
2026-03-09 15:27:20 +0100

    small fix

--- src/imperandi/ingest/parse.py
+++ src/imperandi/ingest/parse.py
@@ -45,6 +45,9 @@ logger = logging.getLogger(__name__)
 DEFAULT_CHECKPOINT_EVERY_ROWS = 10_000
 DEFAULT_CHECKPOINT_EVERY_SEC = 5 * 60  # 5 minutes
 
+PARSE_WORKER_BATCH_SIZE = 16384
+PARSE_CHECKPOINT_SCHEMA_VERSION = 1
+
 # Make reading tolerant of non-conformant values
 config.settings.reading_validation_mode = config.IGNORE  # or config.WARN
 
