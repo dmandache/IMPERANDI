5fa2ca3b06fbde56854481286c1d2b546c3531f6
dmandache <diana.mandache00@gmail.com>
2026-03-09 15:22:07 +0100

    add manifest to ingest in test pipeline

--- tests/pipeline.sh
+++ tests/pipeline.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Default value for DIR_PATH is "./tests/data" if not provided as an argument
 DIR_PATH="${1:-"./tests/data"}"
 
-imperandi ingest "${DIR_PATH}/IRCAD_DICOM" --snapshot_tags
+imperandi ingest "${DIR_PATH}/IRCAD_DICOM" --snapshot_tags --manifest generic
 imperandi convert "${DIR_PATH}/dicom_index_clean.csv" "${DIR_PATH}/IRCAD_nifti"
 imperandi segment "${DIR_PATH}/nifti_index.csv"
 imperandi phase "${DIR_PATH}/nifti_index.csv"
