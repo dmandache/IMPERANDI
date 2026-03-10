caf1c7c4ffc419800b4b3be9de6c750b82871696
dmandache <diana.mandache00@gmail.com>
2026-03-10 16:22:23 +0100

    add manifest to test pipeline

--- tests/pipeline.ps1
+++ tests/pipeline.ps1
@@ -4,8 +4,8 @@ $DIR_PATH = ".\tests\data"
 
 imperandi ingest "$DIR_PATH\IRCAD_DICOM" --snapshot_tags --manifest generic
 imperandi convert "$DIR_PATH\dicom_index_clean.csv" "$DIR_PATH\IRCAD_nifti"
-imperandi segment "$DIR_PATH\nifti_index.csv"
+imperandi segment "$DIR_PATH\nifti_index.csv" --manifest generic
 imperandi phase "$DIR_PATH\nifti_index.csv"
-imperandi radiomics "$DIR_PATH\nifti_index.csv"
+imperandi radiomics "$DIR_PATH\nifti_index.csv" --manifest generic
 
 echo "Pipeline test completed successfully."
--- tests/pipeline.sh
+++ tests/pipeline.sh
@@ -6,8 +6,8 @@ DIR_PATH="${1:-"./tests/data"}"
 
 imperandi ingest "${DIR_PATH}/IRCAD_DICOM" --snapshot_tags --manifest generic
 imperandi convert "${DIR_PATH}/dicom_index_clean.csv" "${DIR_PATH}/IRCAD_nifti"
-imperandi segment "${DIR_PATH}/nifti_index.csv"
+imperandi segment "${DIR_PATH}/nifti_index.csv" --manifest generic
 imperandi phase "${DIR_PATH}/nifti_index.csv"
-imperandi radiomics "${DIR_PATH}/nifti_index.csv"
+imperandi radiomics "${DIR_PATH}/nifti_index.csv" --manifest generic
 
 echo "Pipeline executed successfully!"
